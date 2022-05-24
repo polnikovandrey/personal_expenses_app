1236ccf56947734a78952b9efcfc6fb45d26c613
Andrey Polnikov <polnikov.andrey@gmail.com>
2022-05-25 00:02:57 +0300

    + fix transaction list height (hardcode)

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,7 +11,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 300,
+      height: 500,
       child: _userTransactions.isEmpty
           ? Column(
               children: [
